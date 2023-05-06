178d4a268ebbbbf4cbc6bff98dca8854612c4fde
Devanshi Mistry <109334957+devanshi1611@users.noreply.github.com>
2023-05-06 10:12:29 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         
         <div className="new">
-            <p className="title"><img className="logo_img" src="to-do-list.png" alt="OOPS!!"></img>ToDo</p>
+            <p className="title">ToDo</p>
         </div>
 
     )
